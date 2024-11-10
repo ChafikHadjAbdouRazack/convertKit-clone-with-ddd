a964a70b4efb0ddda0bf31d6c5cf701fcc795290
Chafik HADJ ABDOU RAZACK <chafikhadjabdourazack@gmail.com>
2024-11-10 23:43:06 +0100

    refactor: update tag dto

--- src/Domain/Subscriber/DTO/TagData.php
+++ src/Domain/Subscriber/DTO/TagData.php
@@ -2,9 +2,11 @@
 
 namespace Domain\Subscriber\DTO;
 
+use Spatie\LaravelData\Concerns\WithDeprecatedCollectionMethod;
 use Spatie\LaravelData\Data;
 
 class TagData extends Data
 {
+    use WithDeprecatedCollectionMethod;
     public function __construct(public readonly ?int $id, public readonly string $title) {}
 }
