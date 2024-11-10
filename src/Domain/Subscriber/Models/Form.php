497ad58e4a073f64467f0eab4361caa548b55404
Chafik HADJ ABDOU RAZACK <chafikhadjabdourazack@gmail.com>
2024-11-10 18:48:54 +0100

    feat: add form model

---
+++ src/Domain/Subscriber/Models/Form.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Domain\Subscriber\Models;
+
+use Domain\Shared\Models\BaseModel;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Form extends BaseModel
+{
+
+    public function subscribers(): HasMany
+    {
+        return $this->hasMany(Subscriber::class);
+    }
+}
