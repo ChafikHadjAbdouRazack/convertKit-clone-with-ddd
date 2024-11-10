9b1bc902b1474bc4e3ed8f0dfd6dce4bd6f86401
Chafik HADJ ABDOU RAZACK <chafikhadjabdourazack@gmail.com>
2024-11-10 18:49:05 +0100

    feat: add tag model

---
+++ src/Domain/Subscriber/Models/Tag.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Domain\Subscriber\Models;
+
+use Domain\Shared\Models\BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tag extends BaseModel
+{
+
+    public function subscribers(): BelongsToMany
+    {
+        return $this->belongsToMany(Subscriber::class);
+    }
+}
