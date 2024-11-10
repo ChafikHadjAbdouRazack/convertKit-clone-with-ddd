b42eb8c3dc0dddfe2525955afdc9683eca4dcfdd
Chafik HADJ ABDOU RAZACK <chafikhadjabdourazack@gmail.com>
2024-11-10 18:48:42 +0100

    feat: add subscriber model

---
+++ src/Domain/Subscriber/Models/Subscriber.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Domain\Subscriber\Models;
+
+use Domain\Shared\Models\BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Subscriber extends BaseModel
+{
+
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function form(): BelongsTo{
+        return $this->belongsTo(Form::class)->withDefault();
+    }
+
+}
