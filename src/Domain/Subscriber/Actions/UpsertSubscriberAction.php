af2be27c9c54f46c0d887c5dde014abd0d098e09
Chafik HADJ ABDOU RAZACK <chafikhadjabdourazack@gmail.com>
2024-11-10 23:43:25 +0100

    feat: add subscriber action

---
+++ src/Domain/Subscriber/Actions/UpsertSubscriberAction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Domain\Subscriber\Actions;
+
+use Domain\Shared\Models\User;
+use Domain\Subscriber\DTO\SubscriberData;
+use Domain\Subscriber\Models\Subscriber;
+
+class UpsertSubscriberAction
+{
+  public static function execute(SubscriberData $data, User $user): Subscriber{
+      $subscriber = Subscriber::updateOrCreate([
+          'id' => $data->id,
+      ],[
+          ...$data->all(),
+          'form_id' => $data->form?->id,
+          'user_id' => $user->id
+      ]);
+      $subscriber->tags()->sync($data->tags->toCollection()->collect()->pluck('id'));
+      return $subscriber->load('tags','form');
+  }
+}
